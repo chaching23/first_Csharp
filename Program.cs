068b7057d439f2702cd0da2ecaf9a29874cbb902
kian chachi <chachi@kians-MacBook-Pro.local>
2019-09-20 09:21:28 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        //Average
+
+
+
+
+
+
+        //Sigma
+
+        for var(i=0;i<Array.length;i++)
+        sum+=
+        
+
+
+
+
+
+
+
+        }
+    }
+}
